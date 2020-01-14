83cce33743e48c98d9aaed8adef078227c0e3c4d
Zachary <davidadallas25@live.com>
2020-01-13 23:01:08 -0700

    Finale of Milestone1

--- Assets/Scrpits/SpriteChanger.cs
+++ Assets/Scrpits/SpriteChanger.cs
@@ -8,21 +8,9 @@ public class SpriteChanger : MonoBehaviour
     public SpriteRenderer theRenderer;
     //Variable for color
     public Color spriteColor;
-    //Variable for transform 
-    public Transform tf;
-    //Variable for the Speed of the object
-    public float speedscale = .2f;
-    //Speed of object on x axis
-    public float speedx = 1;
-    //Speed of object on y axis
-    public float speedy = 1;
-    //Speed of object on z axis
-    public float speedz = 1;
     // Start is called before the first frame update
     void Start()
     {
-                 //Change the color value of our SpriteRenderer component to red
-                 //theRenderer.color = Color.red;
         //Change the color from our color picker to that the alpha is one
         spriteColor.a = 1.0f;
         if(theRenderer != null){
@@ -31,19 +19,11 @@ public class SpriteChanger : MonoBehaviour
         }
         //load the SpriteRenderer component from the same object the component is on
         theRenderer = gameObject.GetComponent<SpriteRenderer>();
-        //Get transform Component
-        tf = GetComponent<Transform>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Creates a new vector based on the magnitude given to it by the variables
-        Vector3 myVector = new Vector3(speedx, speedy, speedz);
-        //makes the vector shange it's speed based on the speed given to it by the speedscale variable
-        tf.position = tf.position + (myVector * speedscale);
-        //Move up a certain distance per frame
-        //tf.position += Vector3.up * .02f;
+
     }
 }
---
+++ Assets/Scrpits/SpriteFacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFacement : MonoBehaviour
+{
+    public Transform Transform;
+    public float movementspeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.up * Time.deltaTime * movementspeed;
+    }
+}
